ac3590081369fe9c3e07729eab331e9b35404254
Janice-M <janiceink001@gmail.com>
2024-06-17 13:18:45 +0300

    Task List Item

---
+++ src/components/TaskListItem.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native"
+
+
+function TaskListItem() {
+    return(
+        <View>
+            <Text>
+                
+            </Text>
+        </View>
+
+
+    )
+}
