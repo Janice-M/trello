ec00fe5957e85dbc023e4f44e33b0ffe76c6a495
Janice-M <janiceink001@gmail.com>
2024-06-17 13:10:06 +0300

    trello part one

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bib!</Text>
+      <Text>bib !</Text>
       <StatusBar style="auto" />
     </View>
   );
