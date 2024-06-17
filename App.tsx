4ff0cb0c3f2d0fca7ca0f628c25e3eb63e029e81
Janice-M <janiceink001@gmail.com>
2024-06-17 14:14:17 +0300

    tasklist

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import TaskListItem from './src/components/TaskListItem';
-
+import TaskList from './src/components/TaskList';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      
       <View style={{padding: 10 }}>
+      <TaskList />
       <TaskListItem task={{description: 'First Task'}}/>
       <TaskListItem task={{description: 'Second Task'}}/>
       <TaskListItem task={{description: 'Third Task'}}  />
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function TaskList() {
+
+    return(
+        <View>
+            <Text>To Do</Text>
+        </View>
+    )
+
+
+
+}
