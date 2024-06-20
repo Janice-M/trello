55fcc9f6da3f51e20ce14ed3c29952d418104ea4
Janice-M <janiceink001@gmail.com>
2024-06-20 15:03:11 +0300

    gradient

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -6,13 +6,15 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function Taskboard () {
     return(
         <View style={{padding: 10 }}>
-            <TaskList />
+            
             <LinearGradient
         // Button Linear Gradient
         colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={styles.button}>
+        style={{position:"absolute", top:0, bottom:0, left:0, right:0,}}>
        
       </LinearGradient>
+
+      <TaskList />
       </View>
     )
 }
