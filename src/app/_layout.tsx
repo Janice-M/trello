599d544304a65e25978068caca0f897595fdb090
Janice-M <janiceink001@gmail.com>
2024-07-01 12:53:09 +0300

    dark theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { Text} from "react-native";
-import {Slot, Stack} from 'expo-router'
+import {Slot, Stack} from 'expo-router';
+import { ThemeProvider, DarkTheme } from '@react-navigation/native'
 
 export default function RootLayOut(){
     
     return (
-        <>
+        <ThemeProvider value={DarkTheme}>
         <Stack screenOptions={{}}>
 
         </Stack>
 
         <StatusBar style="light" />
-        </>
+        </ThemeProvider>
     );
     
     
