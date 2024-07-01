2bdbd620291620f3a984c273a0a8a709c037428c
Janice-M <janiceink001@gmail.com>
2024-07-01 12:17:45 +0300

    creating details of every single task

---
+++ src/app/details.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from  'react-native';
+
+
+const TaskDetails = () => {
+    return (
+        <View>
+            <Text>
+                details
+            </Text>
+        </View>
+    )
+}
