5e0736c2126bee96bed89e8dacdae7699da0cc6d
Janice-M <janiceink001@gmail.com>
2024-07-10 15:09:41 +0300

    schema edits

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,14 @@
 import { BSON, ObjectSchema,Realm } from "realm";
 
 export class Task extends Realm.Object<Task> {
-    _id!: BSON.ObjectId;
-    name!: string;
+    _id: BSON.ObjectId = new BSON.ObjectId();
+    description: string;
+    isComplete: boolean = false;
+    createdAt: Date = new Date();
+    owner_id!:string;
+
+
+    static primaryKey = '_id';
     
     
   }
