4de7d58ed6f811bf2ae9da322ab8512beebe7765
Janice-M <janiceink001@gmail.com>
2024-07-10 15:11:16 +0300

    user is required

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -5,7 +5,7 @@ export class Task extends Realm.Object<Task> {
     description!: string;
     isComplete: boolean = false;
     createdAt: Date = new Date();
-    owner_id!:string;
+    user_id!:string;
 
 
     static primaryKey = '_id';
