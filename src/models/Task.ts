2faf6e5a4af36a1590b171cab1b33f093769c58e
Janice-M <janiceink001@gmail.com>
2024-07-10 14:59:36 +0300

    build dev

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,6 +3,7 @@ import { BSON, ObjectSchema,Realm } from "realm";
 export class Task extends Realm.Object<Task> {
     _id!: BSON.ObjectId;
     name!: string;
+    description :string;
     static schema: ObjectSchema = {
       name: 'Task',
       properties: {
