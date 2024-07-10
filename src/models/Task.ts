cc2c01c2b3485f41f1830b1761e6476b953327ba
Janice-M <janiceink001@gmail.com>
2024-07-10 15:10:43 +0300

    description is required

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,7 +2,7 @@ import { BSON, ObjectSchema,Realm } from "realm";
 
 export class Task extends Realm.Object<Task> {
     _id: BSON.ObjectId = new BSON.ObjectId();
-    description: string;
+    description!: string;
     isComplete: boolean = false;
     createdAt: Date = new Date();
     owner_id!:string;
