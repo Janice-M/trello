667cbb7fc1e6ea2468dc0a391e11091716dfcb79
Janice-M <janiceink001@gmail.com>
2024-07-10 15:05:49 +0300

    schema edits

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,14 +3,7 @@ import { BSON, ObjectSchema,Realm } from "realm";
 export class Task extends Realm.Object<Task> {
     _id!: BSON.ObjectId;
     name!: string;
-    description :string;
-    static schema: ObjectSchema = {
-      name: 'Task',
-      properties: {
-        _id: 'objectId',
-        name: {type: 'string', indexed: 'full-text'},
-      },
-      primaryKey: '_id',
-    };
+    
+    
   }
   
